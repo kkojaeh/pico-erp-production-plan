b8801f16b301e56cb050edb02760026c2efea1e4
Ethan <kkojaeh@gmail.com>
2019-04-15 09:20:13 +0900

    jms 활성화

--- impl/src/main/java/pico/erp/production/plan/ProductionPlanApplication.java
+++ impl/src/main/java/pico/erp/production/plan/ProductionPlanApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import pico.erp.ComponentDefinition;
 import pico.erp.production.plan.ProductionPlanApi.Roles;
@@ -20,6 +21,7 @@ import pico.erp.shared.data.Role;
 @Slf4j
 @SpringBootComponent("production-plan")
 @EntityScan
+@EnableJms
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
 @EnableJpaRepositories
